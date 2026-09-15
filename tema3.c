c8d222be953d754f11f0952126d5ed0795aaf132
Oscar Dominguez Alonso <iamoscaar@gmail.com>
2026-09-15 19:43:02 +0200

    fix

--- tema3.c
+++ tema3.c
@@ -2,8 +2,35 @@
 
 int main() {
     
-
+    int n1,n2;
     printf("Ingrese el primer numero: ");
-    return 0;
+    scanf("%d", &n1);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &n2);
 
+/*
+    
+    if(n1>n2){
+        printf("El primer numero es mayor que el segundo\n");
+    }else if(n1<n2){
+        printf("El segundo numero es mayor que el primero\n");
+    }else{
+        printf("Los numeros son iguales\n");
+    }
+*/
+
+    int n3;
+    printf("Ingrese el tercer numero: ");
+    scanf("%d", &n3);
+    
+    
+    if (n3 == n1)
+    {
+    
+        
+    }
+    
+    
+
+    return 0;
 }
